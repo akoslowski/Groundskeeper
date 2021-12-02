02d3217694115317371321ae76426cd71f58b45a
andreas.koslowski <andreas.koslowski@xing.com>
2021-12-02 23:57:19 +0100

    Add feature to automatically open playground

--- Sources/playground/main.swift
+++ Sources/playground/main.swift
@@ -21,11 +21,16 @@ struct AddPage: ParsableCommand {
     @Argument(help: "Name of the new playground page")
     var pageName: String?
 
+    @Flag(inversion: .prefixedNo, help: "Automatically open the playground after adding a page")
+    var autoOpen: Bool = true
+
     func run() throws {
         let url = URL(fileURLWithPath: playgroundPath)
 
         let g = Groundskeeper(fileSystem: FileManager.default)
         let targetURL = try g.addPage(playgroundURL: url, pageName: pageName)
+
+        if autoOpen { openWithXcode(targetURL) }
 
         print(targetURL.path)
     }
@@ -38,13 +43,32 @@ struct Create: ParsableCommand {
     @Option(help: "URL to the output path where the playground will be created. Defaults to ~/Downloads")
     var outputPath: String?
 
+    @Flag(inversion: .prefixedNo, help: "Automatically open the playground after creation")
+    var autoOpen: Bool = true
+
     func run() throws {
         guard let url = URL(string: outputPath ?? "~/Downloads") else { throw GroundskeeperError.invalidURL }
         let g = Groundskeeper(fileSystem: FileManager.default)
         let targetURL = try g.createPlayground(with: name, outputURL: url)
 
+        if autoOpen { openWithXcode(targetURL) }
+
         print(targetURL.path)
     }
+}
+
+func openWithXcode(_ url: URL) {
+    runProcess(launchPath: "/usr/bin/xed", arguments: [url.path])
+}
+
+func runProcess(launchPath: String, arguments: [String] = []) {
+    let task = Process()
+    task.launchPath = launchPath
+    task.arguments = arguments
+    task.standardOutput = Pipe()
+    task.standardError = Pipe()
+    task.launch()
+    task.waitUntilExit()
 }
 
 GroundskeeperCommand.main()
