a3329cad3edd3b53619425726333006bacdeb958
andreas.koslowski <andreas.koslowski@xing.com>
2021-12-05 14:17:01 +0100

    Update description

--- Sources/playground/main.swift
+++ Sources/playground/main.swift
@@ -18,13 +18,13 @@ struct Create: ParsableCommand {
     @Argument(help: "Name of the new playground. If no name is given, a random name will be used")
     var name: String?
 
-    @Option(help: "URL to the output path where the playground will be created. Defaults to ~/Downloads")
+    @Option(help: "URL to the output path where the playground will be created")
     var outputPath: String = "~/Downloads"
 
     @Flag(inversion: .prefixedNo, help: "Automatically open the playground after creation using 'xed'")
     var xed: Bool = true
 
-    @Option(help: "Source code template for the playground page. Options are 'swift', 'swiftui' or a URL pointing to a file")
+    @Option(help: "Source code template for the playground page. Options are 'swift', 'swiftui' or a URL pointing to content")
     var template: SourceCodeTemplate = .swift
 
     func run() throws {
@@ -49,7 +49,7 @@ struct AddPage: ParsableCommand {
     @Flag(inversion: .prefixedNo, help: "Automatically open the playground after adding a page using 'xed'")
     var xed: Bool = true
 
-    @Option(help: "Source code template for the playground page. Options are 'swift', 'swiftui' or a URL pointing to a file")
+    @Option(help: "Source code template for the playground page. Options are 'swift', 'swiftui' or a URL pointing to content")
     var template: SourceCodeTemplate = .swift
 
     func run() throws {
