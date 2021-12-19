61151c0235972bae1e0beeebe3a0e51176867a9a
andreas.koslowski <andreas.koslowski@xing.com>
2021-12-19 20:14:12 +0100

    Remove pages from contents.xcplayground as it is not required (anymore)

--- Sources/Groundskeeper/FileContents.swift
+++ Sources/Groundskeeper/FileContents.swift
@@ -3,22 +3,6 @@ import XMLCoder
 
 // MARK: - Playground - content.xcplayground -
 
-struct Page: Codable, DynamicNodeEncoding {
-    static func nodeEncoding(for key: CodingKey) -> XMLEncoder.NodeEncoding {
-        .attribute
-    }
-
-    let name: String
-}
-
-struct Pages: Codable {
-    enum CodingKeys: String, CodingKey {
-        case values = "page"
-    }
-
-    let values: [Page]
-}
-
 public enum TargetPlatform: String, Codable {
     case ios, macos
 }
@@ -34,29 +18,18 @@ struct Content: Codable, DynamicNodeEncoding {
     }
 
     enum CodingKeys: String, CodingKey {
-        case version, buildActiveScheme, pages
+        case version, buildActiveScheme
         case targetPlatform = "target-platform"
     }
 
     let version: String
     let targetPlatform: TargetPlatform
     let buildActiveScheme: Bool
-    let pages: Pages
 
-    init(version: String = "6.0", targetPlatform: TargetPlatform, buildActiveScheme: Bool = true, pages: [Page]) {
+    init(version: String = "6.0", targetPlatform: TargetPlatform, buildActiveScheme: Bool = true) {
         self.version = version
         self.targetPlatform = targetPlatform
         self.buildActiveScheme = buildActiveScheme
-        self.pages = .init(values: pages)
-    }
-
-    func adding(_ page: Page) throws -> Self {
-        .init(
-            version: version,
-            targetPlatform: targetPlatform,
-            buildActiveScheme: buildActiveScheme,
-            pages: pages.values + [page]
-        )
     }
 }
 
