65e575da12028c7e99a140023619677b1145e33f
andreas.koslowski <andreas.koslowski@xing.com>
2021-12-21 22:04:09 +0100

    Move defaults to framework

--- Sources/Groundskeeper/UserDefaults/Defaults.swift
+++ Sources/Groundskeeper/UserDefaults/Defaults.swift
@@ -1,36 +1,35 @@
 import Foundation
-import Groundskeeper
 
-protocol KeyValueStorage {
+public protocol KeyValueStorage {
     func string(forKey: String) -> String?
 }
 
-struct Defaults {
-    static let suiteName = "com.groundskeeper.playground"
+public struct Defaults {
+    public static let suiteName = "com.groundskeeper.playground"
 
-    private let storage: KeyValueStorage
+    let storage: KeyValueStorage
 
-    init?(storage: KeyValueStorage? = UserDefaults(suiteName: suiteName)) {
+    public init?(storage: KeyValueStorage? = UserDefaults(suiteName: suiteName)) {
         guard let storage = storage else { return nil }
         self.storage = storage
     }
 
-    var playgroundOutputPath: String? {
+    public var playgroundOutputPath: String? {
         storage.string(forKey: "GKPlaygroundOutputPath")
     }
 
-    var playgroundTargetPlatform: String? {
+    public var playgroundTargetPlatform: String? {
         storage.string(forKey: "GKPlaygroundTargetPlatform")
     }
 
     // MARK: -
 
-    var outputURL: FileURL? {
+    public var outputURL: FileURL? {
         guard let value = playgroundOutputPath else { return nil }
         return try? FileURL(path: value)
     }
 
-    var targetPlatform: TargetPlatform? {
+    public var targetPlatform: TargetPlatform? {
         guard let value = playgroundTargetPlatform else { return nil }
         return TargetPlatform(argument: value)
     }
